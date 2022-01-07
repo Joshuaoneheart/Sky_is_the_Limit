9bfe8c6d4c2fcf02b3c28fc0c15007411a70ee04
Joshuaoneheart <newheart0561@gmail.com>
2022-01-07 13:51:37 +0800

    CMU_MOSEI download.sh

--- data/ASRGLUE/download.sh
+++ data/ASRGLUE/download.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#gdown --id 1sQgCxS9HNXxmuiuf7jP1LfrFqE4mi4RD 
+gdown --id 1sQgCxS9HNXxmuiuf7jP1LfrFqE4mi4RD 
 unzip ASRGLUE.zip
+rm -rf ASRGLUE.zip
---
+++ data/CMU_MOSEI/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#gdown --id 1gzRgGRB7HOTfnoqNaRXfQHBC_HOamCPD
+#unzip CMU_MOSEI.zip
+#unzip CMU_MOSEI/Transcript.zip
+#unzip CMU_MOSEI/Segmented_Audio.zip
+#rm -rf CMU_MOSEI.zip
+#rm -rf CMU_MOSEI/Transcript.zip
+#rm -rf CMU_MOSEI/Segmented_Audio.zip
+mv CMU_MOSEI/* .
+mv Segmented_Audio Audio
+mv Segmented Transcript
+rmdir CMU_MOSEI
