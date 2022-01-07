30ddf7da12df41cdf1cf746ddde09955b48a5e66
Joshuaoneheart <newheart0561@gmail.com>
2022-01-07 13:36:50 +0800

    ASRGLUE and FSC's download.sh

---
+++ data/ASRGLUE/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#gdown --id 1sQgCxS9HNXxmuiuf7jP1LfrFqE4mi4RD 
+unzip ASRGLUE.zip
---
+++ data/FSC/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gdown --id 1BHgcu_jHV5yMN5pddFJdsiEmcTIwIdyL
+tar -zxvf FSC.tar.gz
+mv fluent_speech_commands_dataset FSC 
+rm -rf FSC.tar.gz
